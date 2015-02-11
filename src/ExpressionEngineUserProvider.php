d73924715bd29e61467577e4dd2aa3a9df407e83
Amo Chohan <amo.chohan@gmail.com>
2015-02-11 09:41:40 +0000

    Create ExpressionEngineUserProvider.php

---
+++ src/ExpressionEngineUserProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DrawMyAttention\ExpAuth;
+
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+use Illuminate\Auth\EloquentUserProvider;
+use Illuminate\Contracts\Hashing\Hasher as HasherContract;
+
+class ExpressionEngineUserProvider extends EloquentUserProvider{
+
+
+    public function __construct(HasherContract $hasher, $model)
+    {
+        parent::__construct($hasher, $model);
+    }
+
+    /**
+     * Validate a user against the given credentials.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  array  $credentials
+     * @return bool
+     */
+    public function validateCredentials(UserContract $user, array $credentials)
+    {
+        $plain                  = $credentials['password'];
+        $options['salt']        = $user->salt;
+        $options['byte_size']   = strlen($user->getAuthPassword());
+        return $this->hasher->check($plain, $user->getAuthPassword(), $options);
+    }
+
+}
