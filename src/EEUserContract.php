1aaa02d59d8ea73aed9e5800b66d4c08d3287cfa
Jonathan Baker <jbaker@baker-arts.com>
2015-09-08 17:18:26 -0500

    Removed reference to App\User. User model now must implement EEUserContract in place of AuthenticatableContract

---
+++ src/EEUserContract.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DrawMyAttention\ExpAuth;
+
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+
+interface EEUserContract extends UserContract {
+    /* Intentionally left blank */
+}
--- src/ExpressionEngineUserProvider.php
+++ src/ExpressionEngineUserProvider.php
@@ -2,10 +2,8 @@
 
 namespace DrawMyAttention\ExpAuth;
 
-use Illuminate\Contracts\Auth\Authenticatable as UserContract;
 use Illuminate\Auth\EloquentUserProvider;
 use Illuminate\Contracts\Hashing\Hasher as HasherContract;
-use App\User;
 
 class ExpressionEngineUserProvider extends EloquentUserProvider{
 
@@ -18,18 +16,18 @@ class ExpressionEngineUserProvider extends EloquentUserProvider{
     /**
      * Validate a user against the given credentials.
      *
-     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
-     * @param  array  $credentials
+     * @param EEUserContract|\Illuminate\Contracts\Auth\AuthenticatableContract $user
+     * @param  array $credentials
      * @return bool
      */
-    public function validateCredentials(UserContract $user, array $credentials)
+    public function validateCredentials(EEUserContract $user, array $credentials)
     {
-        $plain                  = $credentials['password'];
-        $options = array();
-        if ($user instanceof User) {
-            $options['salt']        = $user->salt;
-            $options['byte_size']   = strlen($user->getAuthPassword());
-        }
+        $plain = $credentials['password'];
+        $options = array(
+            'salt' => $user->salt,
+            'byte_size' => strlen($user->getAuthPassword())
+        );
+
         return $this->hasher->check($plain, $user->getAuthPassword(), $options);
     }
 
